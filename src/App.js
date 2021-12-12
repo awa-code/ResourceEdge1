997d04d0c367aeeb6918eb15373924a714ec4c45
awa-code <66476113+awa-code@users.noreply.github.com>
2021-12-12 22:40:49 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Hr from "./components/Hr";
+import Admin from "./components/Admin";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Hr />
+      <Admin />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from "react";
+import heroImg from "../assets/hero-img.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="section-center hero-center">
+        <div className="hero-text">
+          <h1 className="title">
+            Throw paperwork into the trash where it belongs.
+          </h1>
+          <p className="paragraph-text">
+            Eliminate all the hassle involved in managing people operations by
+            automating it.
+          </p>
+        </div>
+        <div className="hero-img">
+          <img src={heroImg} alt="" className="hero-photo img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
